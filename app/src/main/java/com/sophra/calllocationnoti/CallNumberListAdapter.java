dbe392935acc669228e1edfd19c5821e459913f4
chungddong <chungman010918@gmail.com>
2024-03-09 04:25:02 +0900

    리사이클러 어댑터 코드 작업

--- app/src/main/java/com/sophra/calllocationnoti/CallNumberListAdapter.java
+++ app/src/main/java/com/sophra/calllocationnoti/CallNumberListAdapter.java
@@ -1,6 +1,54 @@
 package com.sophra.calllocationnoti;
 
-public class CallNumberListAdapter {
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+public class CallNumberListAdapter extends RecyclerView.Adapter<CallNumberListAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public CallNumberListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CallNumberListAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView CallNumber;
+        TextView CallMemo;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            CallNumber = itemView.findViewById(R.id.list_CallNum);
+            CallMemo = itemView.findViewById(R.id.list_CallMemo);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    Log.d("CLN", "롱클릭함");
+                    return false;
+                }
+            });
+        }
+
+
+    }
 }
 
 
